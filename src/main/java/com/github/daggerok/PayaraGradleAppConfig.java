94d40e3b26564ff5d66fb156da729510ea07efd8
Maksim Kostromin <daggerok@gmail.com>
2019-02-15 21:38:54 +0200

    rename

--- src/main/java/com/github/daggerok/PayaraGradleAppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.daggerok;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationScoped
-@ApplicationPath("v1")
-public class PayaraGradleAppConfig extends Application { }
